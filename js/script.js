2736142c7747c45d77fe134203d1ed4cfc97a480
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-05 15:49:07 +0200

    stampata lista

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
+const { createApp } = Vue
+
 createApp({
     data() {
-        return [
-            {
-                text: 'fare la spesa',
-                isDone: true,
-            },
-            {
-                text: 'portare la macchina dal meccanico',
-                isDone: true,
-            },
-            {
-                text: 'visita psicologo',
-                isDone: false,
-            },
-            {
-                text: 'comprare un lanciafiamme',
-                isDone: false,
-            },
-            {
-                text: 'Completare esercizio di programmazione',
-                isDone: false,
-            }
-        ]
+        return {
+            todoList: [
+                {
+                    text: 'fare la spesa',
+                    done: true,
+                },
+                {
+                    text: 'portare la macchina dal meccanico',
+                    done: true,
+                },
+                {
+                    text: 'visita psicologo',
+                    done: false,
+                },
+                {
+                    text: 'comprare un lanciafiamme',
+                    done: false,
+                },
+                {
+                    text: 'Completare esercizio di programmazione',
+                    done: false,
+                }
+            ]
+        }
     },
+
     methods: {
 
     }
